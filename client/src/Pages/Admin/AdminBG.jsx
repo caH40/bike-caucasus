7a1e3887261f06fbf21765f442232d427011f12c
Alexander Berezhnev <freelancerbav@gmail.com>
2023-07-04 10:22:44 +0300

    final fixes to run on the server

--- client/src/Pages/Admin/AdminBG.jsx
+++ client/src/Pages/Admin/AdminBG.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import classes from '../PagesCss/AdminBG.module.css';
 
 const AdminBG = () => {
-	return (
-		<section className={classes.wrapper}>
-			<img className={classes.img} src="images/admin-bg-small.png" alt="bg" />
-		</section>
-	);
+  return (
+    <section className={classes.wrapper}>
+      <img className={classes.img} src="assets/images/admin-bg-small.png" alt="bg" />
+    </section>
+  );
 };
 
 export default AdminBG;
